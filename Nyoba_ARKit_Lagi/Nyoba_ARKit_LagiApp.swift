717e11274977fc3deb6f3542168abba176af7d18
Renaldi <renaldi.antonio19@gmail.com>
2024-04-26 23:19:57 +0700

    Initial Commit

---
+++ Nyoba_ARKit_Lagi/Nyoba_ARKit_LagiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nyoba_ARKit_LagiApp.swift
+//  Nyoba_ARKit_Lagi
+//
+//  Created by Renaldi Antonio on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Nyoba_ARKit_LagiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
