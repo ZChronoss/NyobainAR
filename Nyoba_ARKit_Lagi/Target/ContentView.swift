fd6d503514fc2f6c04b4c919746eb0e8f2811bf5
Renaldi <renaldi.antonio19@gmail.com>
2024-04-27 02:18:51 +0700

    Ada ubah beberapa: 1. Ilangin tombol 2. Nambahin model bisa pake tap gesture

--- Nyoba_ARKit_Lagi/Target/ContentView.swift
+++ Nyoba_ARKit_Lagi/Target/ContentView.swift
@@ -15,49 +15,49 @@ struct ContentView: View {
     var body: some View {
         CustomARViewRepresentable()
             .ignoresSafeArea()
-            .overlay(alignment: .bottom){
-                ScrollView(.horizontal){
-                    HStack{
-                        Button{
-                            ARManager.shared.actionStream.send(.removeAllAnchors)
-                        }label: {
-                            Image(systemName: "trash")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 40, height: 40)
-                                .padding()
-                                .background(.red)
-                                .clipShape(.rect(cornerRadius: 16))
-                                .tint(.black)
-                        }
-                        
-                        ForEach(models, id: \.self){ model in
-                            Button{
-                                ARManager.shared.actionStream.send(.placeBlock(model: model))
-                            }label: {
-                                Image(model)
-                                    .scaledToFill()
-                                    .frame(width: 40, height: 40)
-                                    .padding()
-                                    .background(.regularMaterial)
-                                    .clipShape(.rect(cornerRadius: 16))
-                            }
-                        }
-                        
-                        Button{
-                            ARManager.shared.actionStream.send(.addBackgroundImage)
-                        }label: {
-                            Image("Mountain_BG")
-                                .scaledToFill()
-                                .frame(width: 40, height: 40)
-                                .padding()
-                                .background(.regularMaterial)
-                                .clipShape(.rect(cornerRadius: 16))
-                        }
-                    }
-                    .padding()
-                }
-            }
+//            .overlay(alignment: .bottom){
+//                ScrollView(.horizontal){
+//                    HStack{
+//                        Button{
+//                            ARManager.shared.actionStream.send(.removeAllAnchors)
+//                        }label: {
+//                            Image(systemName: "trash")
+//                                .resizable()
+//                                .scaledToFit()
+//                                .frame(width: 40, height: 40)
+//                                .padding()
+//                                .background(.red)
+//                                .clipShape(.rect(cornerRadius: 16))
+//                                .tint(.black)
+//                        }
+//                        
+//                        ForEach(models, id: \.self){ model in
+//                            Button{
+//                                ARManager.shared.actionStream.send(.placeBlock(model: model))
+//                            }label: {
+//                                Image(model)
+//                                    .scaledToFill()
+//                                    .frame(width: 40, height: 40)
+//                                    .padding()
+//                                    .background(.regularMaterial)
+//                                    .clipShape(.rect(cornerRadius: 16))
+//                            }
+//                        }
+//                        
+//                        Button{
+//                            ARManager.shared.actionStream.send(.addBackgroundImage)
+//                        }label: {
+//                            Image("Mountain_BG")
+//                                .scaledToFill()
+//                                .frame(width: 40, height: 40)
+//                                .padding()
+//                                .background(.regularMaterial)
+//                                .clipShape(.rect(cornerRadius: 16))
+//                        }
+//                    }
+//                    .padding()
+//                }
+//            }
     }
 }
 
--- Nyoba_ARKit_Lagi/Target/CustomARView.swift
+++ Nyoba_ARKit_Lagi/Target/CustomARView.swift
@@ -11,6 +11,10 @@ import RealityKit
 import ARKit
 
 class CustomARView: ARView {
+    
+    var moveToLocation: Transform = Transform()
+    var modelEntity: ModelEntity?
+    
     required init(frame frameRect: CGRect) {
         super.init(frame: frameRect)
     }
@@ -22,37 +26,166 @@ class CustomARView: ARView {
     convenience init(){
         self.init(frame: UIScreen().bounds)
         
+        planeDetection()
+//        coaching tu yg ada tulisan "Move iPhone to Start"
+        addCoaching()
+        loadModel(model: "Tiger")
+//        createBackground()
+//        subscribeToActionStream()
+        
+//        butuh ini keknya gegara kita tu make ARView nya bukan di aplikasi AR, tp di aplikasi swiftui biasa
+        self.installGestures([.all], for: modelEntity! as HasCollision)
+        
+//        ini intinya buat kasih tau kalo user ada tap di layar
+        /*
+         Param:
+         1. target: target object yg datanya mau dikirim
+         2. action: jalanin function apa setelah ada gesture (disini tap gesture). functionnya harus ada @objc
+         */
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
+        self.addGestureRecognizer(tap)
+    }
+    
+    func planeDetection(){
+//        yang ada disini gw kurang tau juga, baca2 aja wkwkw. gw asal nambah soalnya
+        self.automaticallyConfigureSession = true
+        
         let config = ARWorldTrackingConfiguration()
         config.planeDetection = [.horizontal,.vertical]
         config.environmentTexturing = .automatic
-        self.session.run(config)
         
-        addCoaching()
-//        createBackground()
-        subscribeToActionStream()
+//        yg debugOptions ini buat keluarin ijo2 di plane yg ke detect
+        self.debugOptions = .showAnchorGeometry
+        self.session.run(config)
+    }
+    
+//    ini load model
+    func loadModel(model: String){
+        modelEntity = try! Entity.loadModel(named: model + ".usdz")
+        modelEntity?.setScale(SIMD3(x: 0.1, y: 0.1, z: 0.1), relativeTo: modelEntity)
     }
     
 //    cancellable is needed whenever your app is using Combine
     private var cancellables: Set<AnyCancellable> = []
     
-    func subscribeToActionStream(){
-        ARManager.shared.actionStream
-            .sink { [weak self] action in
-                switch action {
-                case .placeBlock(let model):
-                    self?.placeBlock(ofModel: model)
-                    
-                case .removeAllAnchors:
-                    self?.scene.anchors.removeAll()
-                    
-                case .addBackgroundImage:
-                    self?.createBackground()
-                }
-                
-            }
-            .store(in: &cancellables) // this will make sure that the subscription is kept alive even when the function is done
+//    func subscribeToActionStream(){
+//        ARManager.shared.actionStream
+//            .sink { [weak self] action in
+//                switch action {
+//                case .placeBlock(let model):
+//                    self?.placeBlock(ofModel: model)
+//                    self?.playAnimation()
+//                    
+//                case .removeAllAnchors:
+//                    self?.scene.anchors.removeAll()
+//                    
+//                case .addBackgroundImage:
+//                    self?.createBackground()
+//                }
+//                
+//            }
+//            .store(in: &cancellables) // this will make sure that the subscription is kept alive even when the function is done
+//    }
+    
+//    handle tap function
+//    @objc itu kasih tau kalo nih function tu punya objective c
+    @objc
+    func handleTap(recognizer: UITapGestureRecognizer? = nil){
+//        2D tap location
+        guard let tapLocation = recognizer?.location(in: self) else { return }
+        
+//        raycast = 2d to 3d
+        let results = self.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal)
+        
+        if let firstResult = results.first {
+//            3d position
+            let worldPos = simd_make_float3(firstResult.worldTransform.columns.3)
+            
+//            place object
+            placeBlock(position: worldPos)
+        }
+    }
+
+    func addCoaching(){
+        let coachingOverlay = ARCoachingOverlayView()
+
+        // Goal is a field that indicates your app's tracking requirements.
+        coachingOverlay.goal = .tracking
+             
+        // The session this view uses to provide coaching.
+        coachingOverlay.session = self.session
+             
+        // How a view should resize itself when its superview's bounds change.
+        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        self.addSubview(coachingOverlay)
+        
     }
     
+//    ini buat bikin background png doang
+    func createBackground(){
+        let screen = MeshResource.generatePlane(width: 1, depth: 1)
+        var material = UnlitMaterial()
+        
+        if let baseResource = try? TextureResource.load(named: "Mountain_BG"){
+            let baseColor = MaterialParameters.Texture(baseResource)
+            
+//            ini yang bikin background di gambar ilang
+            material.color = .init(tint: UIColor(white: 1, alpha: 0.99), texture: baseColor)
+        }
+        
+        let imageModelEntity = ModelEntity(mesh: screen, materials: [material])
+        let anchor = AnchorEntity(plane: .vertical)
+        
+        anchor.addChild(imageModelEntity)
+        
+        scene.addAnchor(anchor)
+    }
+    
+    func placeBlock(position: SIMD3<Float>){
+        // create an anchor entity and add the model to it
+        let anchorEntity = AnchorEntity(world: position)
+        anchorEntity.addChild(modelEntity!)
+        
+        playAnimation()
+        
+        scene.addAnchor(anchorEntity)
+    }
+    
+    func playAnimation(){
+        if let entityAnimation = modelEntity?.availableAnimations.first{
+//            play animation
+            modelEntity?.playAnimation(entityAnimation.repeat(), transitionDuration: 5, startsPaused: false)
+        }
+    }
+    
+    func moveEntity(direction: String){
+        switch direction{
+        case "forward":
+//            ini maju kedepan. translation itu buat kasih tau kalo maju kedepan nambahin vector z nya 20
+            moveToLocation.translation = (modelEntity?.transform.translation)! + simd_float3(x: 0, y: 0, z: 20)
+            modelEntity?.move(to: moveToLocation, relativeTo: modelEntity, duration: 5)
+            
+//            nambahin animasi jalan kalo bisa wkwk
+        
+        case "back":
+            moveToLocation.translation = (modelEntity?.transform.translation)! + simd_float3(x: 0, y: 0, z: -20)
+            modelEntity?.move(to: moveToLocation, relativeTo: modelEntity, duration: 5)
+            
+        case "left":
+            let rotateAngle = simd_quatf(angle: GLKMathDegreesToRadians(90), axis: SIMD3(x: 0, y: 1, z: 0))
+            modelEntity?.setOrientation(rotateAngle, relativeTo: modelEntity)
+            
+        case "right":
+            let rotateAngle = simd_quatf(angle: GLKMathDegreesToRadians(90), axis: SIMD3(x: 0, y: 1, z: 0))
+            modelEntity?.setOrientation(rotateAngle, relativeTo: modelEntity)
+        default:
+            print("Ga gerak mas")
+        }
+    }
+    
+    
+//  ---------------- EXAMPLE ONLY ----------------
     func configurationExample(){
 //        Tracks the device relative to it's environment
         let configuration = ARWorldTrackingConfiguration()
@@ -100,69 +233,7 @@ class CustomARView: ARView {
         let anchor = AnchorEntity()
         anchor.addChild(entity)
     }
+//  ---------------- EXAMPLE ONLY ----------------
     
-    func addCoaching(){
-        let coachingOverlay = ARCoachingOverlayView()
-
-        // Goal is a field that indicates your app's tracking requirements.
-        coachingOverlay.goal = .tracking
-             
-        // The session this view uses to provide coaching.
-        coachingOverlay.session = self.session
-             
-        // How a view should resize itself when its superview's bounds change.
-        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-
-        self.addSubview(coachingOverlay)
-        
-    }
-    
-    func createBackground(){
-        let screen = MeshResource.generatePlane(width: 1, depth: 1)
-        var material = UnlitMaterial()
-        
-        if let baseResource = try? TextureResource.load(named: "Mountain_BG"){
-            let baseColor = MaterialParameters.Texture(baseResource)
-            material.color = .init(tint: UIColor(white: 1, alpha: 0.99), texture: baseColor)
-        }
-        
-        let imageModelEntity = ModelEntity(mesh: screen, materials: [material])
-        let anchor = AnchorEntity(plane: .vertical)
-        
-//        imageModelEntity.setPosition(SIMD3(x: 0, y: 0, z: 1.5), relativeTo: anchor)
-//        imageModelEntity.transform.rotation = simd_quatf(angle: -90, axis: [1, 0, 0])
-        
-        anchor.addChild(imageModelEntity)
-        
-        scene.addAnchor(anchor)
-    }
-    
-    func placeBlock(ofModel model: String){
-//        let block = MeshResource.generateBox(size: 1)
-//        let material = SimpleMaterial(color: UIColor(color), isMetallic: false)
-//        let entity = ModelEntity(mesh: block, materials: [material])
-//        
-//        let anchor = AnchorEntity(plane: .horizontal)
-//        anchor.addChild(entity)
-//        
-//        scene.addAnchor(anchor)
-        
-        // load the model from the app's asset catalog
-        let modelEntity = try! ModelEntity.load(named: model + ".usdz")
-        if(model == "Tiger"){
-            modelEntity.setScale(SIMD3(x: 0.1, y: 0.1, z: 0.1), relativeTo: modelEntity)
-        }
-        
-        if let entityAnimation = modelEntity.availableAnimations.first{
-//            play animation
-            modelEntity.playAnimation(entityAnimation.repeat(), transitionDuration: 5, startsPaused: false)
-        }
-        
-        // create an anchor entity and add the model to it
-        let anchorEntity = AnchorEntity(plane: .horizontal)
-        anchorEntity.addChild(modelEntity)
-        
-        scene.addAnchor(anchorEntity)
-    }
     
 }
